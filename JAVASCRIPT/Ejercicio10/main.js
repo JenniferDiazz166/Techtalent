994f974dde964e33c54709cf5b808ddfef852d57
Jennifer Diaz Cruz <jenniferdiazcruz16@gmail.com>
2024-04-05 12:34:45 +0200

    ejercicio10

---
+++ JAVASCRIPT/Ejercicio10/main.js
@@ -0,0 +1,13 @@
+/*FOR*/
+
+for (let step=0; step <10; step++){
+    console.log("Protect your energy");
+}
+
+/*WHILE*/
+
+let empezar= 0;
+while(empezar < 10 ){
+    console.log("i love code");
+    empezar++;
+}
