2b4df08d6140d37dcd37afe80e0e0f9a1bd240f5
Jennifer Diaz Cruz <jenniferdiazcruz16@gmail.com>
2024-04-05 13:31:45 +0200

    ejercicio11

---
+++ JAVASCRIPT/Ejercicio11/main.js
@@ -0,0 +1,13 @@
+/*parte1*/
+const array =['a', 'b', 'c', 'd', 'e'];
+array.forEach((element) => console.log(element));
+
+/*parte2*/
+
+let numero =5;
+while (numero > -1){
+    console.log(numero);
+    numero--;
+}
+
+
