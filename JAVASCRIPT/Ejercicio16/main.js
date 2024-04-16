253be412f135e4094c71b576f35217e8a373bcc1
Jennifer Diaz Cruz <jenniferdiazcruz16@gmail.com>
2024-04-16 12:34:44 +0200

    ejercicio1

---
+++ JAVASCRIPT/Ejercicio16/main.js
@@ -0,0 +1,9 @@
+function changeImage (){
+    document.getElementById("carta1").style.display= "none";
+    document.getElementById("cambiocarta").style.display= "block";
+}
+
+function restoreImage (){
+    document.getElementById("carta1").style.display="block";
+    document.getElementById("cambiocarta").style.display="none";
+}
