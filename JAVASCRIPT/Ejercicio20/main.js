45b2d82045786b771ea4e314132e74a788d95e2f
Jennifer Diaz Cruz <jenniferdiazcruz16@gmail.com>
2024-04-30 13:47:04 +0200

    ejercicio20

---
+++ JAVASCRIPT/Ejercicio20/main.js
@@ -0,0 +1,28 @@
+//Parte1
+document.getElementById("boton").addEventListener("click", () => {
+  var audio = document.getElementById("sonido");
+  audio.play();
+});
+
+// Parte2
+document.getElementById("boton-2").addEventListener("click", () => {
+  var audio = document.getElementById("sonido-2");
+  audio.play();
+});
+
+//Parte3
+document.getElementById("boton-3").addEventListener("click", () => {
+  var audio = document.getElementById("sonido-3");
+  audio.play();
+});
+
+//Parte4
+document.getElementById("boton-4").addEventListener("click", () => {
+  var audio = document.getElementById("sonido-4");
+  audio.play();
+});
+//Parte5
+document.getElementById("boton-5").addEventListener("click", () => {
+  var audio = document.getElementById("sonido-5");
+  audio.play();
+});
